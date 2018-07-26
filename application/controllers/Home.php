989d61b30013fb63f2ef7f190c5287c79919556b
Ariopambudi <ariopambudi04@gmail.com>
2018-07-26 19:28:18 +0700

    MVC

---
+++ application/controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_data_crud');
+	}
+
+	public function index()
+	{
+		$data1['query'] = $this->m_data_crud->Get_crud();
+		$this->load->view('header');
+		$this->load->view('main_crud', $data1);
+	}
+
+	public function tambah()
+	{
+		$this->load->view('header');
+		$this->load->view('main_tambah_artikel', $data1);
+	}
+
+}
